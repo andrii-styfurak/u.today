7b33c7089195946c6aa6b337f5f2237b19b54567
anstif <stifurak@gmail.com>
2018-11-21 03:31:32 +0200

    category pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,24 @@ const app = new Vue({
   el: "#app",
   data: {
     currency: "usd",
-    activeSearch: false
+    activeSearch: false,
+    showMore: false
+  },
+  methods: {
+    tabsSub(e) {
+      e.target.parentNode.classList.toggle("tabs__item--sub-open");
+    },
+    accordionOpen(e) {
+      e.target.parentNode.classList.toggle("accordion--open");
+    },
+    selectOpen(e) {
+      let select = e.target.parentNode.classList;
+      let isСurrent = select.contains("select--open");
+      let currentOpenSelect = document.querySelector(".select--open");
+
+      currentOpenSelect && !isСurrent && (currentOpenSelect.classList.remove("select--open"));
+
+      select.toggle("select--open");
+    }
   }
 });
